b31d0ed8ba18a8fba6765e9e5db19f41a2d4bed2
DUSHIME Dan Paul <danpauldushime03@gmail.com>
2024-03-25 10:54:27 -0400

    initial

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,7 +2,7 @@
 
 def add_integer(a, b=98):
     def add_numbers(a, b):
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("a must be an integer or float and b must be an integer or float")
     a = int(a)
     b = int(b)
