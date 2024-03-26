716cd8aa1121d00873676ca8320f17741c16ec60
DUSHIME Dan Paul <d.dushime@alustudent.com>
2024-03-26 09:49:27 -0700

    updates

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,42 @@
 #!/usr/bin/python3
-
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)):
-        raise TypeError(:"a must be an integer or a float")
+    """
+    This function adds two integers.
 
-     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
-    a = int(a)
-    b = int(b)
-    return a + b
+    Args:
+    a: The first integer or float to add. Must be convertible to an integer.
+    b: The second integer or float to add. Must be convertible to an integer. Defaults to 98.
+
+    Returns:
+    The sum of a and b, as an integer.
+
+    Doctest:
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100)
+    198
+    >>> add_integer(1.5, 2.5)
+    3
+    >>> add_integer(-1, -2)
+    -3
+    >>> add_integer('a', 1)
+    Traceback (most recent call last):
+    ...
+    TypeError: a must be an integer
+    >>> add_integer(1, 'b')
+    Traceback (most recent call last):
+    ...
+    TypeError: b must be an integer
+    >>> add_integer('a', 'b')
+    Traceback (most recent call last):
+    ...
+    TypeError: a must be an integer
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
 
 
