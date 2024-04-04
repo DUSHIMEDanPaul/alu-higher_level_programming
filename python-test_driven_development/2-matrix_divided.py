fcc61ae296d7124aa078fc5c1611a5168e850b4b
DUSHIME Dan Paul <d.dushime@alustudent.com>
2024-04-04 11:41:28 -0700

    provided new updates

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
 """
-This module '2-matrix_divided.py' divides elements of a matrix. 
-Elements of the matrix will be either integers or floats, 
-also elements of the same row will be of the same size.
+This is a Matrix Divided function
 """
+
+
 def matrix_divided(matrix, div):
-    if not all(isinstance(row, list) for row in matrix) or not all(isinstance(item, (int, float)) for row in matrix for item in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    # Add the rest of your code here
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-      raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div,(int,float)):
-       raise  TypeError("div must be a number")
+    """Divides all elements in a matrix"""
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix of integers/floats")
+    size = None
+    for yy in matrix:
+        if type(yy) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if size is None:
+            size = len(yy)
+        elif size != len(yy):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in yy:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
     if div == 0:
-       raise ZeroDivisionError("division by zero")
-    
-    return [[round(item / div, 2) for item in row] for row in matrix]
+        raise ZeroDivisionError("division by zero")
+    return [[round(i / div, 2) for i in yy] for yy in matrix]
