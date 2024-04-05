89e623ccbadf75a34c303a7e0ff15195fe2b098d
DUSHIME Dan Paul <d.dushime@alustudent.com>
2024-04-05 10:11:54 -0700

    new updates

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
+"""Say my name function."""
+
+
 def say_my_name(first_name, last_name=""):
-    """
-    the following function takes two names though it will rejects arguments that are not strings
-    """
-    if not isinstance(first_name, str):
+    """prints my name."""
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is", first_name, last_name)
 
