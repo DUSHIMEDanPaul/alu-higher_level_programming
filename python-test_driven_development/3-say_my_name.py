42776efb9a491bc893a9e4a858dc2ca6b286505e
DUSHIME Dan Paul <d.dushime@alustudent.com>
2024-04-05 09:42:38 -0700

    new updates

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,24 +1,11 @@
-"""
-This module contains a function called say_my_name. The function takes two parameters: 
-first_name and last_name. It prints a greeting using these names. If no last_name is 
-provided, it defaults to an empty string. The function raises a TypeError if either 
-first_name or last_name is not a string.
-
-Example:
-    >>> from module_name import say_my_name
-    >>> say_my_name("John", "Doe")
-    My name is John Doe
-"""
-
-
 #!/usr/bin/python3
+"""Say my name function."""
+
 
 def say_my_name(first_name, last_name=""):
-    """
-    the following function takes two names though it will rejects arguments that are not strings
-    """
-    if not isinstance(first_name, str):
+    """prints my name."""
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print(f"My name is {first_name} {last_name}")
+    print("My name is", first_name, last_name)
