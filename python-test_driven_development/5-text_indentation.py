d1048c8a986961fff4397928a0741001f919ff71
DUSHIME Dan Paul <d.dushime@alustudent.com>
2024-04-11 08:06:49 -0700

    new updates

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    special_chars = ['.', '?', ':']
+    for char in special_chars:
+        text = text.replace(char, char + '\n\n')
+    
+    lines = text.split('\n')
+    lines = [line.strip() for line in lines]
+    text = '\n'.join(lines)
+    
+    print(text)
+
