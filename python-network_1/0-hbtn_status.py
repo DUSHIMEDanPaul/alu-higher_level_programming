d760b73dee9d0c2665941e771086b0e28931f500
DUSHIME Dan Paul <d.dushime@alustudent.com>
2024-04-11 06:26:33 -0700

    new updates

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
+"""__summary__
+- Write a Python script that fetches https://alu-intranet.hbtn.io/status
+- using the urllib package.
 """
-This module fetches https://alu-intranet.hbtn.io/status
-using the urllib package.
-"""
-
 import urllib.request
 
-url = 'http://0.0.0.0:5050/status'
-
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8'))) 
-
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8"))) 
